f2fb5dc5e48db73774ec61a248e45f6b9144966a
Malavan Srikumar <malavan.srikumar@zendesk.com>
2022-01-26 09:35:03 +1100

    Finish day 2 part 2 dive challenge

---
+++ src/day-2-dive/part-2/__tests__/index.test.ts
@@ -0,0 +1,18 @@
+export {};
+const main = require('../index').main
+const instructionsToResults : Array<Array< Array<string> | number >> = [
+  [
+    ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2',], 900
+  ]
+]
+
+describe('main', () => {
+  it('calculates multiplication of the final horizontal position and depth', () => {
+    for ( const instructionsToResult of instructionsToResults ) {
+      const instructions = instructionsToResult[0]
+      const result = instructionsToResult[1]
+      expect(main(instructions)).toEqual(result)
+    }
+  })
+})
+
